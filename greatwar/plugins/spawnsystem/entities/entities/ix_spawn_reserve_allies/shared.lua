0ed4102cbba70d4cfe9a9d0bf6752a00c7e32758
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-25 19:10:48 -0500

    Add files via upload

---
+++ greatwar/plugins/spawnsystem/entities/entities/ix_spawn_reserve_allies/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type = "anim"
+ENT.Base = "ix_spawn_base"
+ENT.PrintName = "Allies Reserve Spawn"
+ENT.Category = "Helix - Trench Warfare"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+ENT.Team = "allies"
+ENT.SpawnType = "reserve"
