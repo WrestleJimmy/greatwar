408a0ec8e1ee24c6419d85ef052e6be8807da08d
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-27 12:05:34 -0500

    Delete greatwar/spawnsystem directory

--- greatwar/spawnsystem/entities/entities/ix_spawn_reserve_axis/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "ix_spawn_base"
-ENT.PrintName = "Axis Reserve Spawn"
-ENT.Category = "Helix - Trench Warfare"
-ENT.Spawnable = true
-ENT.AdminOnly = true
-
-ENT.Team = "axis"
-ENT.SpawnType = "reserve"
