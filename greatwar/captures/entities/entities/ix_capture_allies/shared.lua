d66f076038b8060e4bdc6c6986fd233276895986
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-27 12:05:26 -0500

    Delete greatwar/captures directory

--- greatwar/captures/entities/entities/ix_capture_allies/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type      = "anim"
-ENT.Base      = "ix_capture_base"
-ENT.PrintName = "Capture Point (Allies defends)"
-ENT.Category  = "Helix - Captures"
-ENT.Spawnable = true
-ENT.AdminOnly = true
-
--- Allies defends; axis attacks.
-ENT.Team = "allies"
