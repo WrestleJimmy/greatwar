b923cfb0622a64091261bbe657045eb71cc3311d
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-27 12:04:40 -0500

    Add files via upload

---
+++ greatwar/spawnsystem/entities/entities/ix_spawn_reserve_axis/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type = "anim"
+ENT.Base = "ix_spawn_base"
+ENT.PrintName = "Axis Reserve Spawn"
+ENT.Category = "Helix - Trench Warfare"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+ENT.Team = "axis"
+ENT.SpawnType = "reserve"
