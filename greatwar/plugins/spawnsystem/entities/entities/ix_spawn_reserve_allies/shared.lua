233bd281674219ab258ac8b1b300823ac47c69c0
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-27 21:34:29 -0500

    Delete greatwar/plugins/spawnsystem directory

--- greatwar/plugins/spawnsystem/entities/entities/ix_spawn_reserve_allies/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "ix_spawn_base"
-ENT.PrintName = "Allies Reserve Spawn"
-ENT.Category = "Helix - Trench Warfare"
-ENT.Spawnable = true
-ENT.AdminOnly = true
-
-ENT.Team = "allies"
-ENT.SpawnType = "reserve"
