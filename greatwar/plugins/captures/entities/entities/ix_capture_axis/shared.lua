7e60e5cb596a871a7d3935cb84f980af45e39e68
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-04-29 10:57:15 -0500

    Delete greatwar/plugins/captures directory

--- greatwar/plugins/captures/entities/entities/ix_capture_axis/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type      = "anim"
-ENT.Base      = "ix_capture_base"
-ENT.PrintName = "Capture Point (Axis defends)"
-ENT.Category  = "Helix - Captures"
-ENT.Spawnable = true
-ENT.AdminOnly = true
-
--- Axis defends; allies attack.
-ENT.Team = "axis"
