717a8bb29298cb9175b2705368f01d1ea1e5dd24
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-05-04 10:12:09 -0500

    Delete greatwar/plugins directory

--- greatwar/plugins/entityplacing/sh_plugin.lua
+++
@@ -1,8 +0,0 @@
-
-PLUGIN.name = "Entity Placing"
-PLUGIN.description = "Adds the ability to place entities from your inventory on the ground."
-PLUGIN.author = "pedro.santos53"
-
-ix.util.Include("sv_hooks.lua")
-ix.util.Include("cl_hooks.lua")
-
