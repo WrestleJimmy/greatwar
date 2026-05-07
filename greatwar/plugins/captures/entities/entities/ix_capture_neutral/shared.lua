72c30a101b4e47ec9b08ed48b610717e55721cc3
WrestleJimmy <52684694+WrestleJimmy@users.noreply.github.com>
2026-05-07 09:36:06 -0500

    Delete greatwar/plugins/captures directory

--- greatwar/plugins/captures/entities/entities/ix_capture_neutral/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type      = "anim"
-ENT.Base      = "ix_capture_neutral_base"
-ENT.PrintName = "Capture Point (Neutral / Meeting Engagement)"
-ENT.Category  = "Helix - Captures"
-ENT.Spawnable = true
-ENT.AdminOnly = true
-
-ENT.Team = "neutral"
